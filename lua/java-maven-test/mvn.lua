35f09a350deba8f631f8f032eb7074f3c0b6cc91
josip <josipkeresman@gmail.com>
2024-09-26 19:45:38 +0200

    Update mvn module

--- lua/java-maven-test/mvn.lua
+++ lua/java-maven-test/mvn.lua
@@ -10,30 +10,24 @@ local M = {}
 local function execute_test(test_name)
     local class_name = util.get_java_class()
 
-    vim.cmd("vsplit term://bash")
-
     local mvn_test_command = string.format("mvn test -Dtest=%s#%s", class_name, test_name)
 
-    -- Execute the command and handle stdout/stderr
     vim.fn.jobstart(mvn_test_command, {
         stdout_buffered = true,
         stderr_buffered = true,
 
-        -- Handle stdout (success/failure output)
         on_stdout = function(_, data, _)
             if data then
                 notify.handle_test_output(data, test_name)
             end
         end,
 
-        -- Handle stderr (errors)
         on_stderr = function(_, data, _)
             if data then
                 notify.handle_test_error(data)
             end
         end,
 
-        -- Handle exit (when the test process finishes)
         on_exit = function(_, exit_code, _)
             if exit_code == 0 then
                 notify.test_completed_successfully()
@@ -48,7 +42,7 @@ end
 function M.execute_test_at_cursor()
     local test_name = util.get_test_name_at_cursor()
     if util.is_test_name_valid(test_name) then
-        execute_mvn_test(test_name)
+        execute_test(test_name)
     else
         notify.invalid_test_name(test_name)
     end
