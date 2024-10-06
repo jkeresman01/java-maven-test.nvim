184c3b4f86434fe15c4beb39c5808195e58c5b13
josip <josipkeresman@gmail.com>
2024-10-06 11:19:54 +0200

    Document the mvn module

--- lua/java-maven-test/mvn.lua
+++ lua/java-maven-test/mvn.lua
@@ -3,6 +3,11 @@ local util = require("java-maven-test.util")
 
 local M = {}
 
+-- Executes a specific test method using Maven
+--
+-- @param test_name The name of the test method to execute
+--
+-- @return nil
 function M.execute_test(test_name)
     local class_name = util.get_java_class()
     local mvn_test_command = string.format("mvn test -Dtest=%s#%s", class_name, test_name)
@@ -18,6 +23,13 @@ function M.execute_test(test_name)
     })
 end
 
+-- Executes the test method at the current cursor position
+--
+-- This function retrieves the test name under the cursor and validates it.
+-- If the test name is valid, it will execute the test, otherwise, it will
+-- display an error notification.
+--
+-- @return nil
 function M.execute_test_at_cursor()
     local test_name = util.get_test_name_at_cursor()
 
@@ -28,6 +40,13 @@ function M.execute_test_at_cursor()
     end
 end
 
+-- Executes all tests in the current Java class
+--
+-- If there are any test methods in the current Java class, it will run all
+-- the tests by passing an empty string for the test method. If no tests are found,
+-- it will notify the user.
+--
+-- @return nil
 function M.execute_all_tests_in_class()
     local test_methods = util.get_test_methods()
 
